0a37f0bfcc1487b879452d2f2460bf5027f5a2b3
Pietro Oss <pietrooss92@gmail.com>
2022-05-25 15:39:49 +0200

    add: exercise's javascript 10

--- Javascript/10) - Blocchi condizionali e cicli/1637077374330/exercise-7.js
+++ Javascript/10) - Blocchi condizionali e cicli/1637077374330/exercise-7.js
@@ -1,5 +1,11 @@
 function sumUntil(maxValue) {
-  // ...
+  let result = '';
+	let tot =  0;
+	for (let i = 1; i <= maxValue; i++) {
+		tot = i + tot;
+		result = `${result} + ${i}`;
+	}
+	return `${result} +  =  ${tot}`;
 }
 
 console.log(sumUntil(5));
