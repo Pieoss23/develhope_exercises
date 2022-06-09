087236a598b3354c41f4acc503f4f8f7eaba9c5d
Pietro Oss <pietrooss92@gmail.com>
2022-06-09 10:25:03 +0200

    add: 10 exercises oj js

--- Javascript/40) - Le classi/1637149359976/exercise-7.js
+++ Javascript/40) - Le classi/1637149359976/exercise-7.js
@@ -1,59 +1,42 @@
 class Person {
-  constructor(firstName,lastName,age){
+  constructor(firstName, lastName, age) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
   }
 
-  get firstName(){
-    return this._firstName;
+  get name() {
+    return this.firstName;
   }
 
-  set firstName(value){
-    value = value.trim();
-    if(value === '') {
-      throw 'The first name cannot be empty';
-    }
-    this._firstName = value;
-
-    //value === '' || value === undefined ? console.log('The first name cannot be empty')  : this._firstName = value;
+  set name(value) {
+    return (this.firstName = value);
   }
 
-  get lastName(){
-    return this._lastName;
+  get surname() {
+    return this.lastName;
   }
 
-  set lastName(value){
-    value = value.trim();
-    if(value === ''){
-      throw 'The last name cannot be empty'
-    }
-    this._lastName = value;
-
-    //value === '' || value === undefined ? console.log('The last name cannot be empty')  :  this._lastName = value;
+  set surname(value) {
+    this.lastName = value;
   }
 
-  get age() {
-    return this._age;
+  get ages() {
+    return this.age;
   }
 
-  set age(value) {
-    if(value === 0 || value === undefined){
-      throw 'The field <age> cannot be empty'
-    }
-    this._age = value;
-    
-    //value === 0 || value === undefined ? console.log('The field <age> cannot be undefined')  : this._age = value;
+  set ages(value) {
+    return (this.age = value);
   }
 
-  get fullName(){
-    return `${this.firstName} ${this.lastName}`
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
 
-const person = new Person('Mario', 'Rossi', 25);
+const person = new Person("Mario", "Rossi", 25);
 console.log(person.fullName);
 
-person.firstName = 'Maria';
-person.lastName = 'Verdi';
+person.firstName = "Maria";
+person.lastName = "Verdi";
 console.log(person.fullName);
