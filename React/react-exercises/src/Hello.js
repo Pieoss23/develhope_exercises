86f0de3d470ea1236baafd1058f012236df9d941
Pietro Oss <pietrooss92@gmail.com>
2022-07-19 09:58:36 +0200

    add: ex components-06

--- React/react-exercises/src/Hello.js
+++ React/react-exercises/src/Hello.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 export class Hello extends React.Component {
-    render(){
-        return <h2>Hello, World</h2>
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello, World</h1>
+        <p>What a beautiful day!</p>
+      </div>
+    );
+  }
 }
