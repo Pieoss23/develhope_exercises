ff6f81d531d211b3ddebeb3ee944ac5896a34f0f
Pietro Oss <pietrooss92@gmail.com>
2022-07-26 12:04:49 +0200

    add: ex state-03

--- React/react-exercises/src/Counter.js
+++ React/react-exercises/src/Counter.js
@@ -1,21 +1,34 @@
 import React from "react";
 
 export class Counter extends React.Component {
+  state = {
+    count: this.props.initialValue ?? 0,
+  };
+
   constructor(props) {
     super(props);
-
-    this.state = {
-      count: 0,
-    };
-
     setInterval(() => {
-      this.setState(() => {
-        return { count: this.state.count + 1 };
+      this.setState((state) => {
+        return { count: state.count + this.props.incrementBy };
       });
     }, 1000);
   }
 
   render() {
-    return <h1>Count: {this.state.count}</h1>;
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>;
+      </div>
+    );
   }
 }
+
+/* Modify the Counter component so that the initial value of the counter, the increment 
+interval and the increment amount are passed as props to the component.
+*/
+
+Counter.defaultProps = {
+  timeout: 1000,
+  initialValue: 0,
+  incrementBy: 1,
+};
