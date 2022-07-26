721318a16e47af6e5266472360bd2e794b74536d
Pietro Oss <pietrooss92@gmail.com>
2022-07-26 12:16:49 +0200

    add: ex component-lifecycle-01

--- React/react-exercises/src/Counter.js
+++ React/react-exercises/src/Counter.js
@@ -14,29 +14,25 @@ export class Counter extends React.Component {
     }, 1000);
   }
 
+  componentWillUnmount() {
+    if (this._interval) {
+      clearInterval(this._interval);
+    }
+  }
+
   render() {
     return (
       <div>
-        <CounterDisplay count ={this.state.count} />;
+        <h3>Counter : {this.state.count}</h3>
+        {this.state.count < 6 && <p>This counter is great!</p>}
       </div>
     );
   }
 }
-class CounterDisplay extends React.Component {
-    render() {
-      return (
-        <h1>Counter: {this.props.count}</h1>
-      )
-    }
-  }
 
-/* 
-Extract the h1 tag showing the count state variable into a new component called CounterDisplay
-and render it within the Counter component, passing it the count state variable as a prop.
-*/
 
 Counter.defaultProps = {
   timeout: 1000,
-  initialValue: 0,
   incrementBy: 1,
+  initialValue: 0,
 };
