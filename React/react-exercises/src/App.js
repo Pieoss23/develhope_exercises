cbc9913e5bd023b6f57b08a665d30f4efff7ae76
Pietro Oss <pietrooss92@gmail.com>
2022-07-29 15:30:59 +0200

    add: ex event-01

--- React/react-exercises/src/App.js
+++ React/react-exercises/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ClickCounter } from "./ClickCounter";
 import { Counter } from "./Counter";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
@@ -10,9 +11,14 @@ class App extends React.Component {
         <Hello />
         <Welcome age name = "John" />
         <Counter />
+        <ClickCounter  />
       </div>
     );
   }
 }
 
 export default App;
+
+
+// Create a ClickCounter class component that increments a counter every time a user clicks on a button.
+// Render both the current value of the counter and the button within the ClickCounter component.
---
+++ React/react-exercises/src/ClickCounter.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export class ClickCounter extends React.Component {
+  state = {
+    count: this.props.initialValue,
+  };
+
+  handleCounterIncrement = (event) => {
+      console.log(event)
+      this.setState(state => {
+        return {
+          count: state.count + this.props.incrementBy
+        }
+      })
+    }
+
+  render() {
+    
+    return (
+      <div>
+        <button onClick={this.handleCounterIncrement}>Click me</button>
+        <div>counter: {this.state.count}</div>
+      </div>
+    );
+  }
+}
+
+ClickCounter.defaultProps = {
+  initialValue: 0,
+  incrementBy: 1,
+};
