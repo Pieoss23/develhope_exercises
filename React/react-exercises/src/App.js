fcdeb35e168e2f37a3ade9fbb1a88b435dd6b03d
Pietro Oss <pietrooss92@gmail.com>
2022-08-08 15:53:27 +0200

    add: ex component-composition-02

--- React/react-exercises/src/App.js
+++ React/react-exercises/src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Container>
+        <Container title={"Title from props"}>
           {this.props.children}
         </Container>
       </div>
@@ -15,5 +15,4 @@ class App extends React.Component {
 
 export default App;
 
-// Create a Container component that renders its children within a div tag. 
-// Have the div tag use a white background and a red border, either with a custom class or by using tailwindcss.
+// Modify the Container component so that it can display a title received within the title prop.
--- React/react-exercises/src/Container.js
+++ React/react-exercises/src/Container.js
@@ -4,7 +4,7 @@ export class Container extends React.Component{
     render (){
         return (
             <div className="container">
-                container
+                <h2>{this.props.title}</h2>
                 <div>{this.props.children}</div>
             </div>
         )
