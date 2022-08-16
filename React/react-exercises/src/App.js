55dc92558f87d9a3590337d47b144dd17e005bfc
Pietro Oss <pietrooss92@gmail.com>
2022-08-16 16:25:07 +0200

    add: ex function-components-03

--- React/react-exercises/src/App.js
+++ React/react-exercises/src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <Welcome />
-        <Sum numbers={[18, 21, 27, 30]}/>
+        <Sum />
       </div>
     );
   }
@@ -17,5 +17,5 @@ class App extends React.Component {
 export default App;
 
 
-// Write a Sum function component that receives a numbers prop (expected to be an array of numbers)
-// and renders the sum of all numbers in the array within a h1 tag.
+// What happens if the numbers prop of the Sum component is not set?
+// How can you set a default value for this prop?
--- React/react-exercises/src/Sum.js
+++ React/react-exercises/src/Sum.js
@@ -1,7 +1,7 @@
-export function Sum({numbers}){
+export function Sum({ numbers= [18, 21, 27, 30]}){
     return(
         <div>
-            <h3>Get a sum of this: {numbers}</h3>
+            <h3>Get a sum of this: {numbers.map((num => `${num}, `))}</h3>
             <h1>The sum of all numbers is: {numbers.reduce((number, sum) => sum + number,0)}</h1>
         </div>
     )
