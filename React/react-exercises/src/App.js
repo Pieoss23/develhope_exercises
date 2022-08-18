bc2903919001f4424e4a6e34e1157101dcc78340
Pietro Oss <pietrooss92@gmail.com>
2022-08-18 10:52:54 +0200

    add: ex custom-hook-01

--- React/react-exercises/src/App.js
+++ React/react-exercises/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { GithubUser } from "./GithubUser";
+import { HookCounter } from "./HookCounter";
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="main_container">
-        <GithubUser username='Pieoss23' />
+        <HookCounter />
       </div>
     );
   }
@@ -15,5 +15,5 @@ class App extends React.Component {
 export default App;
 
 
-// Create a GithubUserList component that maintains an array of usernames, showing a GithubUser component for each username entered.
-// The usernames should be added to the array using an input field and a button.
+// Create a custom hook useCounter that keeps track of the state of a counter,
+// and returns the current value of the counter as well as three functions to increment, decrement and reset the counter.
