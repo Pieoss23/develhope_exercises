b8a30284c4b38dc2d9e8ab7a730a287a5156dfd7
Pietro Oss <pietrooss92@gmail.com>
2022-08-18 11:10:00 +0200

    add: ex custom-hook-02

--- React/react-exercises/src/App.js
+++ React/react-exercises/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { HookCounter } from "./HookCounter";
+import { HookLogin } from "./HookLogin";
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="main_container">
-        <HookCounter />
+        <HookLogin />
       </div>
     );
   }
@@ -15,5 +15,5 @@ class App extends React.Component {
 export default App;
 
 
-// Create a custom hook useCounter that keeps track of the state of a counter,
-// and returns the current value of the counter as well as three functions to increment, decrement and reset the counter.
+// Create a custom hook that keeps track of the state of a controlled form with the username and password inputs,
+// and returns the current value of the inputs as well as an event handler to update either input.
