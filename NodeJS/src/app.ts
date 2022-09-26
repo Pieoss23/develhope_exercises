61350a84dd9de588ce9471c0c213676c7ddaab88
Pietro Oss <pietrooss@OMEN-PETRO.localdomain>
2022-09-26 14:48:11 +0200

    add ajv-formats, sinclair and express-json-validator to validate data

--- NodeJS/src/app.ts
+++ NodeJS/src/app.ts
@@ -1,14 +1,31 @@
-import express, { response } from "express";
+import express from "express";
 import "express-async-errors";
+import prisma from "./lib/prisma/client";
+
+import {
+    validate,
+    planetSchema,
+    validationErrorMiddleware,
+    PlanetData,
+} from "./lib/validation";
 
 const app = express();
+app.use(express.json());
 
-app.get("/planets", (request, response) => {
-    const planets = [
-        { name: "Mercury" },
-        { name: "Venus" },
-        // { name: "Jupiter" },
-    ];
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany();
     response.json(planets);
 });
+
+app.post("/planets", validate({ body: planetSchema }), async (request, response) => {
+    const planetData: PlanetData = request.body;
+    const planet = await prisma.planet.create({
+        data: planetData
+    })
+
+    response.status(201).json(planet)
+});
+
+app.use(validationErrorMiddleware);
+
 export default app;
