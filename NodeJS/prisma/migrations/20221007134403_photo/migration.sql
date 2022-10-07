c673b7f33b9998b6fc0f2bcd0b02a4d35c97a078
Pieoss23 <pietrooss92@gmail.com>
2022-10-07 15:52:36 +0200

    fix db

--- NodeJS/prisma/migrations/20221007134403_photo/migration.sql
+++ NodeJS/prisma/migrations/20221007134403_photo/migration.sql
@@ -1,10 +1,4 @@
-/*
-  Warnings:
 
-  - You are about to drop the column `updateAt` on the `Planet` table. All the data in the column will be lost.
-  - Added the required column `updatedAt` to the `Planet` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "Planet" DROP COLUMN "updateAt",
 ADD COLUMN     "photoFilename" VARCHAR(255),
